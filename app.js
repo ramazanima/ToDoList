4d857f9a6bccac682076bcacef6f178943124d3b
Ali Ramazani <ramazanim@berea.edu>
2023-06-09 16:53:48 -0400

    created custome add for the list item

--- app.js
+++ app.js
@@ -3,43 +3,28 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
+
+var items = [];
 
 app.get("/", (request, response) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
+  response.render("list", { kindOfDay: day, newListItem: items });
+});
 
-  response.render("list", {
-    kindOfDay: day 
-  });
+app.post("/", function(request, response) {
+  var item = request.body.newItem;
+  items.push(item);
+  response.redirect("/");
 });
 
 app.listen(3000, () => {
